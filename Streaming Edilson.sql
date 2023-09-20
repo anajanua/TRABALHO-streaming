7058c54de17b61204313cf7e481d0fd5d79e4f73
André <andrerafaelbatista@gmail.com>
2023-09-20 09:44:22 -0300

    revisão periódica

---
+++ Streaming Edilson.sql
@@ -0,0 +1,53 @@
+create database streaming;
+use streaming;
+
+create table filmes(
+	idFilme int not null auto_increment primary key,
+    nomeFilme varchar(50),
+    classificacaoFilme int,
+    dataLancamento date,
+    generoFilme varchar(100),
+    linkFilme varchar(255),
+    gosteiFilme int,
+    naoGosteiFilme int,
+    sinopse varchar(255),
+    dententores varchar(150), 
+    elenco varchar(150),
+    foto blob,
+    trailer blob
+);
+
+create table usuarios(
+	idUsuarios int not null auto_increment primary key,
+    nomeUsuario varchar(50),
+    emailUsuario varchar(60),
+    senhaUsuario varchar(20),
+    fotoUsuario blob,
+    historicoUsuario varchar (100),
+    cartoesUsario varchar(50),
+    premiumUsuario tinyint,
+    venciUsuario date
+    );
+    
+ create table cartao(
+ idCartao int not null auto_increment primary key,
+ numCartao int(16),
+ bandeiraCartao varchar(40),
+ cvvCartao int(3),
+ validadeCartao date,
+ cpf varchar(12),
+ nomeCartao varchar(50),
+ enderecoCartao varchar(100),
+ telCartao int
+ );  
+ 
+ create table servidor(
+ idServidor int not null auto_increment primary key,
+ nomeServidor varchar(100),
+ filmeServidor varchar(50),
+ integServidor varchar(100),
+ tempoServidor time,
+ privServidor varchar(60),
+ caminhoServidor varchar(255)
+ );
+ 
